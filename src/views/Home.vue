2c0ae69d545527227cfdc36b4ed8b7dd9ecba8be
ramamani <ramamani@miteleferico.bo>
2021-08-13 15:23:18 -0400

    Completo formulario con assetFields y enlace BD

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,144 +1,144 @@
 <template>
-  <div class="main__wrapper">
-    <b-container fluid>
-      <b-row>
-        <b-col md="5" >
-          <div class="wrapper--forms">
-            <!-- Show empty state if empty -->
-            <div v-if="forms.length == 0" class="emptyState">
-              <p class="emptyState__p">Arrastrar elementos a esta área</p>
-            </div>
+	<div class="main__wrapper">
+		<b-container fluid>
+			<b-row>
+				<b-col md="5" >
+					<div class="wrapper--forms">
+						<!-- Show empty state if empty -->
+						<div v-if="forms.length == 0" class="emptyState">
+							<p class="emptyState__p">Arrastrar elementos a esta área</p>
+						</div>
 
-            <draggable
-              :list="forms"
-              class="dragArea"
-              :options="sortElementOptions"
-            >
-              <div
-                v-for="(form, index) in forms"
-                :key="index"
-                v-bind="form"
-                class="form__group"
-                :class="{ 'is--active': form === activeForm }"
-              >
-                <!-- En caso de ser seleccionado, muestra el tipo de dato -->
-                <span class="form__selectedlabel">{{ form.fieldType }}</span>
-                <!-- Etiqueta  -->
-                <div @click="editElementProperties(form)">
-                  <label
-                    class="form__label"
-                    v-show="form.hasOwnProperty('label')"
-                    >{{ form.label }}</label
-                  >
-                  <!-- Componente -->
-                  <component
-                    :is="form.fieldType"
-                    :currentField="form"
-                    class="form__field"
-                  >
-                  </component>
-                  <!-- Bloque de ayuda -->
-                  <small
-                    class="form__helpblock"
-                    v-show="form.isHelpBlockVisible"
-                    >{{ form.helpBlockText }}
-                  </small>
-                </div>
+						<draggable
+							:list="forms"
+							class="dragArea"
+							:options="sortElementOptions"
+						>
+							<div
+								v-for="(form, index) in forms"
+								:key="index"
+								v-bind="form"
+								class="form__group"
+								:class="{ 'is--active': form === activeForm }"
+							>
+								<!-- En caso de ser seleccionado, muestra el tipo de dato -->
+								<span class="form__selectedlabel">{{ form.fieldType }}</span>
+								<!-- Etiqueta  -->
+								<div @click="editElementProperties(form)">
+									<label
+										class="form__label"
+										v-show="form.hasOwnProperty('label')"
+										>{{ form.label }}</label
+									>
+									<!-- Componente -->
+									<component
+										:is="form.fieldType"
+										:currentField="form"
+										class="form__field"
+									>
+									</component>
+									<!-- Bloque de ayuda -->
+									<small
+										class="form__helpblock"
+										v-show="form.isHelpBlockVisible"
+										>{{ form.helpBlockText }}
+									</small>
+								</div>
 
-                <!-- Actions list -->
-                <div class="form__actiongroup">
-                  <!-- Boton Mover -->
-                  <b-button
-                    variant="primary"
-                    pill
-                    size="sm"
-                    class="form__actionitem--move"
-                  >
-                    <b-icon icon="arrows-move"> </b-icon>
-                  </b-button>
+								<!-- Actions list -->
+								<div class="form__actiongroup">
+									<!-- Boton Mover -->
+									<b-button
+										variant="primary"
+										pill
+										size="sm"
+										class="form__actionitem--move"
+									>
+										<b-icon icon="arrows-move"> </b-icon>
+									</b-button>
 
-                  <!-- Boton clonar -->
-                  <b-button-group class="form__actionlist">
-                    <b-button
-                      size="sm"
-                      variant="primary"
-                      @click="cloneElement(index, form)"
-                      v-show="!form.isUnique"
-                    >
-                      <b-icon icon="plus-circle-fill"> </b-icon>
-                    </b-button>
-                    <!-- Boton Eliminar -->
-                    <b-button
-                      size="sm"
-                      variant="danger"
-                      @click="deleteElement(index)"
-                    >
-                      <b-icon icon="trash-fill"> </b-icon>
-                    </b-button>
-                  </b-button-group>
-                </div>
-              </div>
-            </draggable>
-          </div>
-        </b-col>
-        <b-col md="7">
-          <b-card>
-            <b-tabs type="border-card" v-model="activeTabForFields">
-              <b-tab title="Elementos">
-                <elements />
-              </b-tab>
+									<!-- Boton clonar -->
+									<b-button-group class="form__actionlist">
+										<b-button
+											size="sm"
+											variant="primary"
+											@click="cloneElement(index, form)"
+											v-show="!form.isUnique"
+										>
+											<b-icon icon="plus-circle-fill"> </b-icon>
+										</b-button>
+										<!-- Boton Eliminar -->
+										<b-button
+											size="sm"
+											variant="danger"
+											@click="deleteElement(index)"
+										>
+											<b-icon icon="trash-fill"> </b-icon>
+										</b-button>
+									</b-button-group>
+								</div>
+							</div>
+						</draggable>
+					</div>
+				</b-col>
+				<b-col md="7">
+					<b-card>
+						<b-tabs type="border-card" v-model="activeTabForFields">
+							<b-tab title="Elementos">
+								<elements />
+							</b-tab>
 
-              <b-tab name="properties" title="Propiedades">
-                <properties
-                  v-show="Object.keys(activeForm).length > 0"
-                ></properties>
-              </b-tab>
+							<b-tab name="properties" title="Propiedades">
+								<properties
+									v-show="Object.keys(activeForm).length > 0"
+								></properties>
+							</b-tab>
 
-              <!-- <b-tab title="Estilos">
-                <theming></theming>
-              </b-tab> -->
-            </b-tabs>
-          </b-card>
-          <b-container>
-            <!-- <el-main> -->
-            <!-- <el-main :style="cssProps"> Añadir la propiedad en caso de estilos personalizados  -->
-            <!-- Se muestra la division donde esta el json resultante -->
-            <!-- </el-main> -->
-            <!-- <el-aside class="wrapper--sidebar" width="450px"> -->
-            
-            
-            <!-- </el-aside> -->
-          </b-container>
-        </b-col>
-      </b-row>
-      <b-row class="mt-5 mb-5" >
-        <br/> Botones de accion
-        <b-button  @click="saveAction">  
-          Guardar
-        </b-button>
-        <b-button variant="danger">
-          Cancelar
-        </b-button>
-      </b-row>
+							<!-- <b-tab title="Estilos">
+								<theming></theming>
+							</b-tab> -->
+						</b-tabs>
+					</b-card>
+					<b-container>
+						<!-- <el-main> -->
+						<!-- <el-main :style="cssProps"> Añadir la propiedad en caso de estilos personalizados  -->
+						<!-- Se muestra la division donde esta el json resultante -->
+						<!-- </el-main> -->
+						<!-- <el-aside class="wrapper--sidebar" width="450px"> -->
+						
+						
+						<!-- </el-aside> -->
+					</b-container>
+				</b-col>
+			</b-row>
+			<b-row class="mt-5 mb-5" >
+				<br/> Botones de accion
+				<b-button  @click="saveAction">  
+					Guardar
+				</b-button>
+				<b-button variant="danger">
+					Cancelar
+				</b-button>
+			</b-row>
 
-      <b-row>        
-        <b-col md="6">
-            forms:
-          <div class="wrapper--snippet">
-            <pre>{{ forms }}</pre>
-          </div>
-        </b-col>
-        <b-col md="6">
-        <br />Actual Form:
-            <div class="wrapper--snippet">
-                <pre>{{ activeForm }}</pre>
-            </div>
-        </b-col>
-      </b-row>
+			<b-row>        
+				<b-col md="6">
+						forms:
+					<div class="wrapper--snippet">
+						<pre>{{ forms }}</pre>
+					</div>
+				</b-col>
+				<b-col md="6">
+				<br />Actual Form:
+						<div class="wrapper--snippet">
+								<pre>{{ activeForm }}</pre>
+						</div>
+				</b-col>
+			</b-row>
 
-    </b-container>
+		</b-container>
 
-  </div>
+	</div>
 </template>
 
 <script>
@@ -146,204 +146,203 @@ import { FormBuilder } from "@/components/form_elements/formbuilder";
 import axios from 'axios'
 
 export default {
-  name: "Home",
+	name: "Home",
 
-  data() {
-    return {
-      payload:{},
-      sortElementOptions: FormBuilder.$data.sortElementOptions,
-    };
-  },
+	data() {
+		return {
+			payload:{},
+			sortElementOptions: FormBuilder.$data.sortElementOptions,
+		};
+	},
 
-  computed: {
+	computed: {
 
-    forms:{
-      get(){
-        return this.$store.state.forms
-      },
-      set(value){
-        this.$store.state.forms = value
-      }      
-    },
-    activeForm:{
-      get(){
-        return this.$store.state.activeForm
-      },
-      set(value){
-        this.$store.state.activeForm = value
-      }      
-    },
-    activeTabForFields:{
-      get(){
-        return this.$store.state.activeTabForFields
-      },
-      set(value){
-        this.$store.state.activeTabForFields = value
-      }      
-    }
-    /* cssProps() {
-      // Return an object that will generate css properties key
-      // to match with the themingVars
-      //
-      // Example output: { '--theme-primary-color': this.themingVars.primaryColor }
-      var result = {},
-        themingVars = this.themingVars;
+		forms:{
+			get(){
+				return this.$store.state.forms
+			},
+			set(value){
+				this.$store.state.forms = value
+			}      
+		},
+		activeForm:{
+			get(){
+				return this.$store.state.activeForm
+			},
+			set(value){
+				this.$store.state.activeForm = value
+			}      
+		},
+		activeTabForFields:{
+			get(){
+				return this.$store.state.activeTabForFields
+			},
+			set(value){
+				this.$store.state.activeTabForFields = value
+			}      
+		}
+		/* cssProps() {
+			// Return an object that will generate css properties key
+			// to match with the themingVars
+			//
+			// Example output: { '--theme-primary-color': this.themingVars.primaryColor }
+			var result = {},
+				themingVars = this.themingVars;
 
-      for (var v in themingVars) {
-        if (themingVars.hasOwnProperty(v)) {
-          var newV = "--theme-" + _.kebabCase(v),
-            suffix = "";
+			for (var v in themingVars) {
+				if (themingVars.hasOwnProperty(v)) {
+					var newV = "--theme-" + _.kebabCase(v),
+						suffix = "";
 
-          // Add px to the value if the default value contain 'px'
-          //if (_.includes(newV, "size")) suffix = "px";
-          //else if (_.includes(newV, "margin")) suffix = "px";
-          //else if (_.includes(newV, "radius")) suffix = "px";
+					// Add px to the value if the default value contain 'px'
+					//if (_.includes(newV, "size")) suffix = "px";
+					//else if (_.includes(newV, "margin")) suffix = "px";
+					//else if (_.includes(newV, "radius")) suffix = "px";
 
-          result[newV] = themingVars[v] + suffix;
-        }
-      }
+					result[newV] = themingVars[v] + suffix;
+				}
+			}
 
-      console.log("result", result);
+			console.log("result", result);
 
-      return result;
-    }, */
-  },
+			return result;
+		}, */
+	},
 
-  mounted() {
-    console.log("forms ->", this.forms);
-    console.log("activeform ->", this.activeForm);
-    console.log("activeTabForFields ->", this.activeTabForFields);
-    console.log ("asset :",this.$store.state.asset_type)
-  },
+	mounted() {
+		console.log("forms ->", this.forms);
+		console.log("activeform ->", this.activeForm);
+		console.log("activeTabForFields ->", this.activeTabForFields);
+		console.log ("asset :",this.$store.state.asset_type)
+	},
 
-  components: FormBuilder.$options.components,
+	components: FormBuilder.$options.components,
 
-  methods: {
-    saveAction(){      
-      this.forms.forEach(async (res) => {
-        var payload = new Object      
-        var opciones = JSON.stringify(res.options)        
-        payload = JSON.parse(JSON.stringify(res))                
-        payload.fk_asset_type = this.$store.state.asset_type
-        payload.created_by = 1
-        payload.updated_by = 1
-        payload.options = opciones
-        console.log(payload)
-        try {
-          await axios.post("/api/v1/asset-field",payload)          
-                    
-        } catch (error) {
-          console.log(error)
-        }        
-      })
-      
-    },
-    deleteElement(index) {
-      //FormBuilder.deleteElement(index);
-      this.activeForm=[]
-      this.activeTabForFields=0
-      this.$delete(this.forms,index)
-    },
+	methods: {
+		saveAction(){      
+			this.forms.forEach(async (res) => {
+				var payload = new Object      
+				var opciones = JSON.stringify(res.options)        
+				payload = JSON.parse(JSON.stringify(res))                
+				payload.fk_asset_type = this.$store.state.asset_type
+				payload.created_by = 1
+				payload.updated_by = 1
+				payload.options = opciones
+				console.log("SaveAction: ",payload)
+				try {
+					console.log(await axios.post(`/api/v1/asset-field/${res.id}`,payload))
+				} catch (error) {
+					console.log(error)
+				}        
+			})
+			
+		},
+		deleteElement(index) {
+			//FormBuilder.deleteElement(index);
+			this.activeForm=[]
+			this.activeTabForFields=0
+			this.$delete(this.forms,index)
+		},
 
-    cloneElement(index, form) {
-      //FormBuilder.cloneElement(index, form);
+		cloneElement(index, form) {
+			//FormBuilder.cloneElement(index, form);
 
-      var cloned = _.cloneDeep(form)
-      this.forms.splice(index,0,cloned)
-    },
+			var cloned = _.cloneDeep(form)
+			this.forms.splice(index,0,cloned)
+		},
 
-    editElementProperties(form) {
-      //FormBuilder.editElementProperties(form);
-      this.$store.state.activeForm = form      
-      this.activeTabForFields = 1
-    },
-  },
+		editElementProperties(form) {
+			//FormBuilder.editElementProperties(form);
+			this.$store.state.activeForm = form      
+			this.activeTabForFields = 1
+		},
+	},
 };
 </script>
 
 <!-- Se añade "scoped" para limitar CSS solo a este componente -->
 <style lang="scss" scoped>
 .main__wrapper {
-  height: 100%;
+	height: 100%;
 }
 
 .emptyState {
-  position: absolute;
-  z-index: 1;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
+	position: absolute;
+	z-index: 1;
+	top: 0;
+	left: 0;
+	right: 0;
+	bottom: 0;
 }
 
 .dragArea {
-  max-width: 600px;
-  margin-left: auto;
-  margin-right: auto;
-  position: relative;
-  min-height: 10px;
-  z-index: 2;
+	max-width: 600px;
+	margin-left: auto;
+	margin-right: auto;
+	position: relative;
+	min-height: 10px;
+	z-index: 2;
 }
 
 .form__selectedlabel {
-  display: none;
-  background: lighten(black, 20%);
-  padding: 3px 5px;
-  color: white;
-  font-size: 12px;
-  position: absolute;
-  top: -18px;
-  right: 15px;
+	display: none;
+	background: lighten(black, 20%);
+	padding: 3px 5px;
+	color: white;
+	font-size: 12px;
+	position: absolute;
+	top: -18px;
+	right: 15px;
 }
 
 .form__actionitem--move {
-  position: absolute;
-  right: -14px;
-  top: 50%;
-  transform: translateY(-50%);
-  visibility: hidden;
+	position: absolute;
+	right: -14px;
+	top: 50%;
+	transform: translateY(-50%);
+	visibility: hidden;
 
-  &:active,
-  &:focus,
-  &:hover {
-    border-color: lighten(black, 50%);
-    background: lighten(black, 50%);
-  }
+	&:active,
+	&:focus,
+	&:hover {
+		border-color: lighten(black, 50%);
+		background: lighten(black, 50%);
+	}
 }
 
 .form__actionlist {
-  position: absolute;
-  margin-top: 10px;
-  visibility: hidden;
-  z-index: 3;
-  right: 0;
-  box-shadow: 4px 4px 0 0 lighten(black, 80%);
-  border-radius: 2px;
+	position: absolute;
+	margin-top: 10px;
+	visibility: hidden;
+	z-index: 3;
+	right: 0;
+	box-shadow: 4px 4px 0 0 lighten(black, 80%);
+	border-radius: 2px;
 }
 
 .form__group {
-  margin-bottom: 25px;
-  border: 2px solid transparent;
-  position: relative;
+	margin-bottom: 25px;
+	border: 2px solid transparent;
+	position: relative;
 
-  &:hover {
-    border-color: lighten(black, 80%);
+	&:hover {
+		border-color: lighten(black, 80%);
 
-    .form__actionitem--move {
-      visibility: visible;
-    }
-  }
+		.form__actionitem--move {
+			visibility: visible;
+		}
+	}
 
-  &.is--active {
-    border-color: lighten(black, 50%);
-    background: lighten(black, 95%);
+	&.is--active {
+		border-color: lighten(black, 50%);
+		background: lighten(black, 95%);
 
-    .form__actionlist {
-      visibility: visible;
-    }
-    .form__selectedlabel {
-      display: inline-block;
-    }
-  }
+		.form__actionlist {
+			visibility: visible;
+		}
+		.form__selectedlabel {
+			display: inline-block;
+		}
+	}
 }
 </style>
--- src/views/asset-type.vue
+++ src/views/asset-type.vue
@@ -1,97 +1,103 @@
 /* eslint-disable no-alert, no-console */
 <template>
-  <div>
-    <!-- ini prueba  -->
-    <b-form-select 
-    v-model="$store.state.asset_type" :options="itemsMarca" value-field="id" text-field="desc"
-    >
-      
-    </b-form-select>    
-    <p>{{ $store.state.asset_type }}</p>
-  </div>
+	<div>
+		<!-- ini prueba  -->
+		<b-form-select
+			v-model="$store.state.asset_type"
+			:options="itemsMarca"
+			value-field="id"
+			text-field="name"
+			@change="fetchFields"
+		>
+		</b-form-select>
+		<p>{{ $store.state.asset_type }}</p>
+	</div>
 </template>
 
 <script>
 //import { core } from "../../config/pluginInit";
-import axios from 'axios'
-import modalForm from './components/modalForm.vue'
+import axios from "axios";
+import modalForm from "./components/modalForm.vue";
 export default {
-  name:"assetType",
-  components: {
-    modalForm
-  },
-  data () {
-    return {
-      // id: "Marca",
-      items: [], // Los items que se cargan de la solicitud
-      itemsMarca: [], // Los items que se cargan de la solicitud
-      itemsModelo: [], // Los items que se cargan de la solicitud
-      marca: {},
-      modelo: {},
-      trademarks: [],      
-    }
-  },
-  computed: {
-    cargadoMarcas () {
-      return !(Object.keys(this.marca).length > 0)
-    },
+	name: "assetType",
+	components: {
+		modalForm,
+	},
+	data() {
+		return {
+			// id: "Marca",
+			items: [], // Los items que se cargan de la solicitud
+			itemsMarca: [], // Los items que se cargan de la solicitud
 
-    totalItems () {
-      return [...this.items]
-    }
-  },
-  mounted () {
-        //core.index();
-        axios.defaults.baseURL="http://localhost:8000"          // TODO Verificar el host 
-        this.cargarMarcas()
-  },
+			marca: {},
+			modelo: {},
+			trademarks: [],
+		};
+	},
+	computed: {
+		cargadoMarcas() {
+			return !(Object.keys(this.marca).length > 0);
+		},
 
-  methods: {
-    cambioMarca (id, obj) {
-      // Si el id es de marca, añadir el objeto marca
-      // console.log(`Cambio Evt: ${id} : ${obj}`);
-      this.marca = obj
-    },
-    async creaMarca (item) {
-      var payload = {}
-      payload.name = item.name
-      // console.log("Create Event", payload)
-      try {
-        await axios.post('/api/v1/trademark', payload)
-        // this.$router.back()
-        this.mensaje('success', 'Exito', 'Modelo actualizado  exitosamente')
-      } catch (error) {
-        this.mensaje('error', 'Error', 'Lo sentimos ha ocurrido un error')
-      }
-    },
+		totalItems() {
+			return [...this.items];
+		},
 
-    async updateMarca (item) {
-      try {
-        await axios.put(`/api/v1/trademark/${parseInt(item.id)}`, item)
-        // this.$router.back()
-        this.mensaje('success', 'Exito', 'Modelo acutalizado exitosamente')
-      } catch (error) {
-        console.error(error)
-        this.mensaje('error', 'Error', 'Lo sentimos ha ocurrido un error')
-      }
-    },
-    deleteMarca (item) {},
-    cambioModelo (id, obj) {
-      // console.log(`Cambio Modelo: ${id} : `);
-      this.modelo = obj[0]
-    },
-    creaModelo (item) {},
-    updateModelo (item) {},
-    deleteModelo (item) {},
+		forms: {
+			get() {
+				return this.$store.state.forms;
+			},
+			set(value) {
+				this.$store.state.forms = value;
+			},
+		},
+		activeForm: {
+			get() {
+				return this.$store.state.activeForm;
+			},
+			set(value) {
+				this.$store.state.activeForm = value;
+			},
+		},
+	},
+	mounted() {
+		//core.index();
+		axios.defaults.baseURL = "http://localhost:8000"; // TODO Verificar el host
+		this.cargarMarcas();
+	},
 
-    cargarMarcas () {
-      
-        this.api = '/api/v1/asset-type/'
-        axios.get(this.api).then((response) => {
-          this.itemsMarca = response.data.data
-          console.log(this.itemsMarca)
-        })      
-    },    
-  }
-}
+	methods: {
+		cambioMarca(id, obj) {
+			// Si el id es de marca, añadir el objeto marca
+			// console.log(`Cambio Evt: ${id} : ${obj}`);
+			this.marca = obj;
+		},
+
+		fetchFields(evt) {
+			console.log("fetch Fields:", evt);
+			this.api = `/api/v1/asset-type/fields/${evt}`;
+			axios.get(this.api).then((response) => {
+                this.forms =response.data.rows[0].asset_fields 
+				console.log(this.forms);
+			});
+		},
+
+		deleteMarca(item) {},
+		cambioModelo(id, obj) {
+			// console.log(`Cambio Modelo: ${id} : `);
+			this.modelo = obj[0];
+		},
+		creaModelo(item) {},
+		updateModelo(item) {},
+		deleteModelo(item) {},
+
+		cargarMarcas() {
+			this.api = "/api/v1/asset-type/";
+			axios.get(this.api).then((response) => {
+				this.itemsMarca = response.data.data;
+				console.log(this.itemsMarca);
+			});
+		},
+	},
+};
 </script>
